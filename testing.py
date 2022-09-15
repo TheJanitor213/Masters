20d960d021e25cdf9a01eda374564b5a51a4b9c0
Bruce Beck <brucebeck995@gmail.com>
2022-09-15 17:56:56 +0200

    Cleaning up

--- testing.py
+++ testing.py
@@ -6,9 +6,80 @@ from tqdm import tqdm
 from scipy.io import wavfile
 from python_speech_features import mfcc
 from string import ascii_uppercase
-
+import joblib
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
 mapping = {float(k): v for k, v in enumerate(ascii_uppercase)}
 
+
+# def pad_audio(data, fs, T=3):
+#     # Calculate target number of samples
+#     N_tar = int(fs * T)
+#     # Calculate number of zero samples to append
+#     shape = data.shape
+#     # Create the target shape
+#     N_pad = N_tar - shape[0]
+#     # print("Padding with %s seconds of silence" % str(N_pad/fs) )
+#     shape = (N_pad,) + shape[1:]
+#     # Stack only if there is something to append
+#     if shape[0] > 0:
+#         if len(shape) > 1:
+#             return np.vstack((np.zeros(shape), data))
+#         else:
+#             return np.hstack((np.zeros(shape), data))
+#     else:
+#         return data
+
+
+# def load_audio(audio_file):
+
+#     samplerate, data = wavfile.read(audio_file)
+#     return pad_audio(data, samplerate)
+
+
+# def extract_features(audio_data):
+#     data = mfcc(audio_data, samplerate=44000, nfft=2048, winfunc=np.hamming)
+#     return data.ravel()
+
+
+
+# # prediction = loaded_classifier.predict(features.flatten().reshape(1,-1))
+
+
+# rootdir = "data_by_subject/"
+
+# all_results = {}
+# letters = {}
+# print("Loading audio...")
+# file_count = sum(len(files) for _, _, files in os.walk(rootdir))
+# with tqdm(total=file_count) as pbar:
+#     for subdir, dirs, files in os.walk(rootdir):
+#         if "6" in subdir or "7" in subdir or "8" in subdir:
+
+#             all_features = []
+
+#             for file in files:
+#                 pbar.update(1)
+#                 sample_rate, audio = wavfile.read(os.path.join(subdir, file))
+#                 data = pad_audio(audio, sample_rate)
+#                 label = os.path.join(subdir, file).split("/")[2]
+#                 features = extract_features(data)
+#                 output = [
+#                     mapping[k] for k in model.predict(features.flatten().reshape(1, -1))
+#                 ]
+#                 if all_results.get(subdir.split("/")[1][-1]):
+#                     if all_results[subdir.split("/")[1][-1]].get(label):
+#                         all_results[subdir.split("/")[1][-1]][label].append(output[0])
+#                     else:
+#                         all_results[subdir.split("/")[1][-1]][label] = [output[0]]
+#                 else:
+#                     all_results[subdir.split("/")[1][-1]] = {label: [output[0]]}
+
+# print(all_results)
+# for k, v in all_results.items():
+#     for k1, v1 in v.items():
+#         print(f"{k1} - {v1.count(k1) / len(v1)}")
 
 def pad_audio(data, fs, T=3):
     # Calculate target number of samples
@@ -29,51 +100,72 @@ def pad_audio(data, fs, T=3):
         return data
 
 
-def load_audio(audio_file):
+def load_audio():
+    rootdir = "data_by_subject/"
+    letters = {}
+    print("Loading audio...")
+    file_count = sum(len(files) for _, _, files in os.walk(rootdir))
+    print(file_count)
+    with tqdm(total=file_count) as pbar:
+        for subdir, dirs, files in tqdm(os.walk(rootdir)):
+            if '6' in subdir or '7' in subdir or '8' in subdir:
+                for file in files:
+                    pbar.update(1)
+                    sample_rate, audio = wavfile.read(os.path.join(subdir, file))
 
-    samplerate, data = wavfile.read(audio_file)
-    return pad_audio(data, samplerate)
+                    data = pad_audio(audio, sample_rate)
+                    label = os.path.join(subdir, file).split("/")[2]
+                    if letters.get(label):
+                        letters[label].append(data)
+                    else:
+                        letters[label] = [data]
+    return letters
+
+characters = load_audio()
+mapping = {v: k for k, v in enumerate(ascii_uppercase)}
+inv_map = {v: k for k, v in mapping.items()}
 
 
 def extract_features(audio_data):
-    data = mfcc(audio_data, samplerate=44100, nfft=2048, winfunc=np.hamming)
-    return data.ravel()
 
+    # Remember that the audio data consists of raw audio wave followed by sample rate
+    # so we need to only take the raw audio wave.
+    output, label_output = [], []
+    print("Extracting features")
+
+    for k, v in tqdm(audio_data.items()):
+        for i in v:
+            label_output.append(k)
+            data = mfcc(i, samplerate=44000, nfft=2048, winfunc=np.hamming)
+            output.append(data.ravel())
+
+    label_output = [mapping[k] for k in label_output]
+    label_output = np.array(label_output)
+    return output, label_output
 
 model = pickle.load(open("classifier_all.pkl", "rb"))
-# prediction = loaded_classifier.predict(features.flatten().reshape(1,-1))
+# Define a function to load the raw audio files
 
+character_features, labels = extract_features(characters)
 
-rootdir = "data_by_subject/"
+X = character_features
+Y = labels
+scaler_filename = "scaler.save"
+scaler = joblib.load(scaler_filename)
 
-all_results = {}
-letters = {}
-print("Loading audio...")
-file_count = sum(len(files) for _, _, files in os.walk(rootdir))
-with tqdm(total=file_count) as pbar:
-    for subdir, dirs, files in os.walk(rootdir):
-        if "6" in subdir or "7" in subdir or "8" in subdir:
+X_test = scaler.transform(X)
+y_pred = model.predict(X_test)
+print(y_pred)
+from sklearn.metrics import confusion_matrix, accuracy_score
+print(accuracy_score(Y, y_pred))
 
-            all_features = []
+df_cm = pd.DataFrame(
+    confusion_matrix(Y, y_pred),
+    index=[i for i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+    columns=[i for i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+)
+plt.figure(figsize=(10, 7))
+all_map = sns.heatmap(df_cm, annot=True)
 
-            for file in files:
-                pbar.update(1)
-                sample_rate, audio = wavfile.read(os.path.join(subdir, file))
-                data = pad_audio(audio, sample_rate)
-                label = os.path.join(subdir, file).split("/")[2]
-                features = extract_features(data)
-                output = [
-                    mapping[k] for k in model.predict(features.flatten().reshape(1, -1))
-                ]
-                if all_results.get(subdir.split("/")[1][-1]):
-                    if all_results[subdir.split("/")[1][-1]].get(label):
-                        all_results[subdir.split("/")[1][-1]][label].append(output[0])
-                    else:
-                        all_results[subdir.split("/")[1][-1]][label] = [output[0]]
-                else:
-                    all_results[subdir.split("/")[1][-1]] = {label: [output[0]]}
-
-print(all_results)
-for k, v in all_results.items():
-    for k1, v1 in v.items():
-        print(f"{k1} - {v1.count(k1) / len(v1)}")
+fig = all_map.get_figure()
+fig.savefig("new_map.png")
--- training.py
+++ training.py
@@ -6,11 +6,11 @@ from sklearn import metrics, preprocessing
 from sklearn import svm
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import GridSearchCV
-
+import scipy.interpolate as interpol
 from scipy.io import wavfile
 import scipy.fftpack as fft
 from scipy.signal import get_window
-
+from sklearn.preprocessing import StandardScaler, StandardScaler
 import IPython.display as ipd
 
 from tqdm import tqdm
@@ -42,6 +42,17 @@ def pad_audio(data, fs, T=3):
         return data
 
 
+def normalize(inSig,outLen):
+	#This function normalizes the audio signal.
+	#It first produces an interp1d structure that readily interpolates between points
+	#Then it sets the size of the space to outLen=200000 points, and interp1d interpolates to fill in gaps
+	#In essence, it takes every audio signal and produces a signal with outLen=200000 data points in it = normalization
+    inSig = np.array(inSig)
+    arrInterpol = interpol.interp1d(np.arange(inSig.size),inSig)
+    arrOut = arrInterpol(np.linspace(0,inSig.size-1,outLen))
+    return arrOut
+
+
 def load_audio():
     rootdir = "data_by_subject/"
     letters = {}
@@ -50,15 +61,23 @@ def load_audio():
     print(file_count)
     with tqdm(total=file_count) as pbar:
         for subdir, dirs, files in tqdm(os.walk(rootdir)):
-            if '1' in subdir or '2' in subdir or '3' in subdir or '4' in subdir or '5' in subdir:
+            if (
+                "1" in subdir
+                or "2" in subdir
+                or "3" in subdir
+                or "4" in subdir
+                or "5" in subdir
+            ):
                 for file in files:
                     pbar.update(1)
                     sample_rate, audio = wavfile.read(os.path.join(subdir, file))
-                    print(sample_rate, audio)
-                    data = pad_audio(audio, sample_rate)
+                    newSig = []
+                    for i in range(len(audio)):newSig.append(audio[i])
+                    newSig = normalize(newSig,200000)
+                    data = pad_audio(newSig, sample_rate)
                     label = os.path.join(subdir, file).split("/")[2]
                     if letters.get(label):
-                        letters[label].append(data)
+                        letters[label].append(data.ravel())
                     else:
                         letters[label] = [data]
     return letters
@@ -69,17 +88,17 @@ characters = load_audio()
 mapping = {v: k for k, v in enumerate(ascii_uppercase)}
 inv_map = {v: k for k, v in mapping.items()}
 
-
 def extract_features(audio_data):
 
     # Remember that the audio data consists of raw audio wave followed by sample rate
     # so we need to only take the raw audio wave.
     output, label_output = [], []
     print("Extracting features")
-
+    
     for k, v in tqdm(audio_data.items()):
         for i in v:
             label_output.append(k)
+
             data = mfcc(i, samplerate=44000, nfft=2048, winfunc=np.hamming)
             output.append(data.ravel())
 
@@ -118,15 +137,10 @@ for i, label in enumerate(Y):
 X_train, X_test, y_train, y_test = train_test_split(
     X, Y, test_size=0.3, random_state=30, stratify=Y
 )
-# X_train_1, X_test_1, y_train_1, y_test_1 = train_test_split(
-#     X_1, Y_1, test_size=0.3, random_state=30, stratify=Y_1
-# )
-# X_train_2, X_test_2, y_train_2, y_test_2 = train_test_split(
-#     X_2, Y_2, test_size=0.3, random_state=30, stratify=Y_2
-# )
-# X_train_3, X_test_3, y_train_3, y_test_3 = train_test_split(
-#     X_3, Y_3, test_size=0.3, random_state=30, stratify=Y_3
-# )
+
+scaler = StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
 
 def custom_dump_svmlight_file(X_train, Y_train, filename):
@@ -151,14 +165,14 @@ def custom_dump_svmlight_file(X_train, Y_train, filename):
             )
 
 
+import joblib
+
+scaler_filename = "scaler.save"
+joblib.dump(scaler, scaler_filename)
+
 custom_dump_svmlight_file(X_train, y_train, "training_data")
 custom_dump_svmlight_file(X_test, y_test, "test_data")
-# custom_dump_svmlight_file(X_train_1, y_train_1, "training_data_1")
-# custom_dump_svmlight_file(X_test_1, y_test_1, "test_data_1")
-# custom_dump_svmlight_file(X_train_2, y_train_2, "training_data_2")
-# custom_dump_svmlight_file(X_test_2, y_test_2, "test_data_2")
-# custom_dump_svmlight_file(X_train_3, y_train_3, "training_data_3")
-# custom_dump_svmlight_file(X_test_3, y_test_3, "test_data_3")
+
 
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.model_selection import GridSearchCV
@@ -168,6 +182,7 @@ from sklearn.metrics import confusion_matrix
 def training(training_data, test_data, file_output_name, params):
     X_train, Y_train = training_data
     X_test, Y_test = test_data
+
     clf = svm.SVC(kernel="rbf", C=params["C"], gamma=params["gamma"])
     clf.fit(X_train, Y_train)
     pred = clf.predict(X_test)
@@ -182,37 +197,14 @@ def training(training_data, test_data, file_output_name, params):
     return confusion_matrix(Y_test, pred)
 
 
-def grid(X, y):
-    C_range = np.logspace(-2, 10, 13, 32)
-    gamma_range = np.logspace(-9, 3, 13, 32)
-    param_grid = dict(gamma=gamma_range, C=C_range)
-    cv = StratifiedShuffleSplit(n_splits=7, test_size=0.3, random_state=42)
-    grid = GridSearchCV(svm.SVC(), param_grid=param_grid, cv=cv)
-    grid.fit(X, y)
+# all_params = grid(X_train, y_train)
+all_params = {"C": 32.0, "gamma": 0.0001220703125}
 
-    print(
-        "The best parameters are %s with a score of %0.2f"
-        % (grid.best_params_, grid.best_score_)
-    )
-    return grid.best_params_
-
-all_params = grid(X_train, y_train)
-# params_1 = grid(X_train_1, y_train_1)
-# params_2 = grid(X_train_2, y_train_2)
-# params_3 = grid(X_train_3, y_train_3)
 
 all_matrix = training(
     (X_train, y_train), (X_test, y_test), "classifier_all.pkl", all_params
 )
-# matrix_1 = training(
-#     (X_train_1, y_train_1), (X_test_1, y_test_1), "classifier_1.pkl", params_1
-# )
-# matrix_2 = training(
-#     (X_train_2, y_train_2), (X_test_2, y_test_2), "classifier_2.pkl", params_2
-# )
-# matrix_3 = training(
-#     (X_train_3, y_train_3), (X_test_3, y_test_3), "classifier_3.pkl", params_3
-# )
+
 
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -227,30 +219,7 @@ plt.figure(figsize=(10, 7))
 all_map = sns.heatmap(df_cm, annot=True)
 
 
-# df_cm = pd.DataFrame(
-#     matrix_1, index=[i for i in "".join(group_1)], columns=[i for i in "".join(group_1)]
-# )
-# plt.figure(figsize=(10, 7))
-# map_1 = sns.heatmap(df_cm, annot=True)
-
-# df_cm = pd.DataFrame(
-#     matrix_2, index=[i for i in "".join(group_2)], columns=[i for i in "".join(group_2)]
-# )
-# plt.figure(figsize=(10, 7))
-# map_2 = sns.heatmap(df_cm, annot=True)
-
-# df_cm = pd.DataFrame(
-#     matrix_3, index=[i for i in "".join(group_3)], columns=[i for i in "".join(group_3)]
-# )
-# plt.figure(figsize=(10, 7))
-# map_3 = sns.heatmap(df_cm, annot=True)
 
 
 fig = all_map.get_figure()
 fig.savefig("all_map.png")
-# fig = map_1.get_figure()
-# fig.savefig("map_1.png")
-# fig = map_2.get_figure()
-# fig.savefig("map_2.png")
-# fig = map_3.get_figure()
-# fig.savefig("map_3.png")
